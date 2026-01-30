ffb21ec5562754b7bc4a13150959ec520b7f1492
jeongis-design <jeongis@gmail.com>
2026-01-30 13:20:20 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ecoinlink-platform/", // 이 줄이 깃허브 주소 연결에 핵심입니다!
+  base: "/ecoinlink-platform/", 
 })
